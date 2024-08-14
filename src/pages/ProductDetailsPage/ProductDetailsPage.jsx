0c9d2546436e597f880897fb6877858361ffbf51
Phamdungcd <phamdungcd108@gmail.com>
2024-08-14 11:41:54 +0700

    update ProductDetailsPage

--- src/pages/ProductDetailsPage/ProductDetailsPage.jsx
+++ src/pages/ProductDetailsPage/ProductDetailsPage.jsx
@@ -1,5 +1,9 @@
 function ProductDetailsPage() {
-  return <div>ProductDetailsPage</div>;
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <p className="text-2xl font-semibold">Page Not Found</p>
+    </div>
+  );
 }
 
 export default ProductDetailsPage;
